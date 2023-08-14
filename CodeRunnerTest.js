876c3f67462d3a39bc8d3bb0304e42275937df31
Marcus Kim <marcuskim1989@gmail.com>
2023-08-13 18:34:14 -0700

    logical operator practices

--- CodeRunnerTest.js
+++ CodeRunnerTest.js
@@ -1,8 +1,14 @@
-console.log('Hello, World!')
-console.log('Hello, dude')
+// console.log('Hello, World!')
+// console.log('Hello, dude')
 
-var person = "John";
-var age = 69;
+// var person = "John";
+// var age = 69;
 
-console.log(person)
-console.log(age)
+// console.log(person)
+// console.log(age)
+
+// var greeting = "What's up, "
+
+// console.log(greeting, person, "aged ", age)
+
+console.log(!(4 != 4))
