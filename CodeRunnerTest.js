143b7aa86fcb5cf2fcb01ea8d306bd11313fe41d
Marcus Kim <marcuskim1989@gmail.com>
2023-08-15 22:07:56 -0700

    my first js function

--- CodeRunnerTest.js
+++ CodeRunnerTest.js
@@ -13,6 +13,18 @@
 
 // console.log(!(4 != 4))
 
-for (i = 1; i <= 100; i++) {
-    console.log(i + " M&M's")
+// for (i = 1; i <= 100; i++) {
+//     console.log(i + " M&M's")
+// }
+
+function letterFinder(word, match) {
+    for (var i = 0; i <= word.length; i++) {
+        if (word[i] == match){
+            console.log("Found the ", match,  "at ", i)
+        } else {
+            console.log("---No match found at ", i)
+        }
+    }
 }
+
+letterFinder("test", "t")
