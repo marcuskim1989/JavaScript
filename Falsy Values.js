512d8c4484d7f72c3ba1ec1b7f8bfea09c8172a5
Marcus Kim <marcuskim1989@gmail.com>
2023-08-20 23:11:14 -0700

    Defensive programming practice

--- Falsy Values.js
+++ Falsy Values.js
@@ -16,4 +16,32 @@ function addTwoNums(a, b) {
     console.log("Error! ",err);
 }
 
-addTwoNums(5, "5")
+//addTwoNums(5, "5")
+
+function letterFinder(word, match) {
+
+    let condition1 = typeof(word) == "string" && word.length >= 2;
+
+    console.log(condition1);
+
+    let condition2 = typeof(match) == "string" && match.length == 1;
+
+    console.log(condition2);
+
+    if(condition1 == true && condition2 == true) {
+        for(let i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //if the current character at position i in the word is equal to the match
+                console.log('Found the', match, 'at', i);
+            } else {
+                console.log('---No match found at', i);
+            }
+        }
+    } else {
+        console.log("Please pass correct arguments to the function");
+    }
+
+    
+}
+
+letterFinder("cat", "c");
