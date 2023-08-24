7d15e283f76a5df31cfa689d1504f8603c7fe659
Marcus Kim <marcuskim1989@gmail.com>
2023-08-23 20:18:36 -0700

    Start at 2, the increased by two while checking that the number is divisible by 2 and n. The first number to meet this condition will be the smallest

---
+++ 2413. Smallest Even Multiple.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+    
+    let i = 2
+
+    while (true) {
+        if (i % 2 == 0 && i % n == 0) {
+            return i
+        }
+
+        i += 2
+    }
+
+};
