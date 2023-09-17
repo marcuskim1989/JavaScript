7954402f11b1c1c44c2b20276d28ad571d902f7a
Marcus Kim <marcuskim1989@gmail.com>
2023-09-16 19:20:08 -0700

    Problem 1967. Loop through patterns. Use includes() method to check whether word includes each pattern

---
+++ 1967. Number of Strings That Appear as Substrings in Word.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function(patterns, word) {
+    
+    let result = 0;
+
+    for (let pattern of patterns) {
+        if (word.includes(pattern)) {
+            result++;
+        }
+    }
+
+    return result;
+
+};
