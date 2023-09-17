5d7918ce812de0fdd013cd684438560d0e067b7e
Marcus Kim <marcuskim1989@gmail.com>
2023-09-17 16:37:59 -0700

    Problem 2824. Use nested loops, ensuring that counter in inner loop always stays ahead of outer loop counter. Sum each pair, compare to target

---
+++ 2824. Count Pairs Whose Sum is Less than Target.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    // loop trailing
+         // loop leading
+            // if leading + trailing < target
+                // increment result
+
+
+    let result = 0;
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
+                result++;
+            }
+        }
+    }
+
+    return result;
+};
